79c64ff396856b908948d87a33de1780b6a6b317
jerry lab <296036136@qq.com>
2018-10-26 20:25:09 +0800

    * initial for Ann

---
+++ models/NN.py
@@ -0,0 +1,61 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+import sklearn
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+
+class NN(object):
+
+    def __init(self, hidden_layer_sizes):
+        pass
+
+    def fit(self, X, y, alpha=0.001, max_iteration=10000):
+        pass
+
+    def predict(self, X):
+        pass
+
+    def show(self):
+        pass
+    
+
+
+
+
+def main(argv):
+    args = parser.parse_args(argv[1:])
+
+
+    data_raw = pd.read_csv('../data/iris.csv', index_col=0)
+    
+    # shuffle
+    data_raw = data_raw.sample(frac=1).reset_index(drop=True)
+
+    # data cleaning
+
+    # data convert 
+    
+    # split dataset
+    cv_size = data_raw.shape[0] * 0.3
+    data, data_cv = data_raw[:cv_size], data_raw[cv_size:]
+
+    # train
+    clf = NN(10)
+    clf.fit(data.drop(columns=['type'], axis=1), data['type'])
+
+    # evaluate
+    y_pred = clf.predict(data_cv.drop(columns=['type'], axis=1))
+    y_true = data_cv['type'].tolist()
+    classification_report(y_true, y_pred)
+
+
+if __name__ == '__main__':
+    import sys, argparse
+
+    parser = argparse.ArgumentParser()
+    # parser.add_argument('-f', '--file', type=str, help='data file, only for csv')
+    
+    main(sys.argv)
